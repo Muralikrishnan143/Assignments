82f4b3413dacf64a59c5d212452444040932a1cf
Muralikrishnan <79559912+Muralikrishnan143@users.noreply.github.com>
2023-02-01 12:24:35 +0530

    Add files via upload

---
+++ day4/Factorial.java
@@ -0,0 +1,14 @@
+package Assignment;
+
+public class Factorial {
+	
+	public static void main(String[] args) {
+
+		int a=1,i=1;
+        for(i=1;i<=5;i++)
+			a=a*i;
+        System.out.println("5 factorial is "+a);
+		
+	}
+
+}
