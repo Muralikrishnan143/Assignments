419898ec00756f9fae5e49a77b80cf6cb5ff7652
Muralikrishnan <79559912+Muralikrishnan143@users.noreply.github.com>
2023-02-01 12:16:37 +0530

    Add files via upload

---
+++ Assignment/PrimeNumber.java
@@ -0,0 +1,24 @@
+package Assignment;
+
+public class PrimeNumber {
+
+public static void main(String[] args) {
+		
+	    int a=13,c;
+	    boolean b =false;
+		
+         for(c=2;c<=2;c++)
+		      {
+		    	  if(c%a==0)
+		    		  b=true;
+		      }
+		      if(b==false)
+		    	  System.out.println("13 is a prime number");
+		      else
+		    	  System.out.println("13 is not a prime number");
+		    	  
+}
+
+}
+
+
